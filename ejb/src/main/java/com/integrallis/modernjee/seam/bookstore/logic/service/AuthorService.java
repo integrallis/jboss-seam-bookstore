1c96ea49726ddd0852e9322d7a442253e1de8aac
Joseph Nusairat <joseph@USS-Defiant-MacBook.local>
2010-04-22 12:40:45 -0700

    Author service

---
+++ ejb/src/main/java/com/integrallis/modernjee/seam/bookstore/logic/service/AuthorService.java
@@ -0,0 +1,22 @@
+package com.integrallis.modernjee.seam.bookstore.logic.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+import com.integrallis.modernjee.seam.bookstore.model.Author;
+
+@Name("authorService")
+public class AuthorService {
+
+	@In
+	EntityManager entityManager;
+	
+	@SuppressWarnings("unchecked")
+	public List<Author> findAllAuthors() {
+		return (List<Author>)entityManager.createQuery("Select a from Author a").getResultList();
+	}
+}
